24df637e82cf846441ba8474be5707bfe8aeb268
Ghislain LEBLANC <ghislain.leblanc@mediactivegroup.com>
2024-11-22 11:05:22 +0100

    add files + readme

--- README.md
+++ README.md
@@ -1,8 +1,43 @@
 # weatherstation
 Hack for Bresser Weatherstation to send data to influxdb
 
-Need composer et influxdata
+Need composer, influxdata
+
+## Intro
+
+The goal of this project is to be able to get the data sent by Bresser Weatherstation and send it to and influxdb database instead of the hardcoded external services.
+The project was originaly made for mysql database and was almost a copy of this project : [original forum discussion](https://community.home-assistant.io/t/weather-station-and-weather-underground-work-around/204443). As we store timeseries data Influxdb is more usefull than Mysql.
+
+To make it working you'll need thew things : 
+* a local DNS
+* an apache2 server with php and composer
+* an influxdb server
+All theses services can be store on the same server 
 
 ---
-# WIP
----
+
+## DNS configuration
+You have to create 2 DNS records to redirect the traffic to your apache2 server :
+* rtupdate.wunderground.com
+* weatherstation.wunderground.com
+
+## influxdb
+* Create a bucket named Weather station (or another name but you'll have to change it in updateweatherstation.php)
+* create a token with Read access to Weatherstation bucket
+
+## apache server
+* Install a standard apache2 server
+* Install php8.2
+* Copy weatherstation.conf to ```/etc/apache2/site-available```
+  * Change the VirtualHost IP
+  * You can change the logs location
+* Copy updateweatherstation.php to ```/var/www/weatherstation/```
+  * Customise the variables for token org and url
+* Install composer : [download](https://getcomposer.org/download/)
+* Install influxdb library ```/var/www/weatherstation/``` in : [influxdb-client-php](https://github.com/influxdata/influxdb-client-php)
+* test that there is no error in php or with the token
+  * ```php /var/www/weatherstation/updateweatherstation.php```
+* activate the website ```a2ensite weatherstation```
+
+
+
--- updateweatherstation.php
+++ updateweatherstation.php
@@ -9,12 +9,12 @@ use InfluxDB2\Point;
 /////////////////////////////////////////
 //InfluxDB connection 
 
-$token = 'vt_pVtKvP4awVbAHyndAl--2-3rbkHydE9EZzMi26tlkVXTO8wpdenhhpo-AGWJTap1vyY_YVok-VtYlnsg-lQ==';
-$org = 'dgidgi.ovh';
-$bucket = 'Weatherstation ';
+$token = 'XXXX';
+$org = 'XXXX.org';
+$bucket = 'Weatherstation';
 
 $client = new Client([
-    "url" => "http://192.168.3.5:8086",
+    "url" => "http://xx.xx.xx.xx:8086",
     "token" => $token,
     "bucket" => $bucket,
     "org" => $org,
@@ -44,69 +44,6 @@ function toHPA( $a) {
 function toMS( $a) {
 	return RoundIt(floatval($a)*3.6);
 }
-
-#// FOUND THIS ON THE NET IN SOME PASTEBIN
-#function wind_cardinal( $degree ) { 
-#  switch( $degree ) {
-#      case ( $degree >= 348.75 && $degree <= 360 ):
-#          $cardinal = "N";
-#      break;
-#      case ( $degree >= 0 && $degree <= 11.249 ):
-#          $cardinal = "N";
-#      break;
-#      case ( $degree >= 11.25 && $degree <= 33.749 ):
-#          $cardinal = "N NO";
-#      break;
-#      case ( $degree >= 33.75 && $degree <= 56.249 ):
-#          $cardinal = "NO";
-#      break;
-#      case ( $degree >= 56.25 && $degree <= 78.749 ):
-#          $cardinal = "O NO";
-#      break;
-#      case ( $degree >= 78.75 && $degree <= 101.249 ):
-#          $cardinal = "O";
-#      break;
-#      case ( $degree >= 101.25 && $degree <= 123.749 ):
-#          $cardinal = "O ZO";
-#      break;
-#      case ( $degree >= 123.75 && $degree <= 146.249 ):
-#          $cardinal = "ZO";
-#      break;
-#      case ( $degree >= 146.25 && $degree <= 168.749 ):
-#          $cardinal = "N";
-#      break;
-#      case ( $degree >= 168.75 && $degree <= 191.249 ):
-#          $cardinal = "Z";
-#      break;
-#      case ( $degree >= 191.25 && $degree <= 213.749 ):
-#          $cardinal = "Z ZW";
-#      break;
-#      case ( $degree >= 213.75 && $degree <= 236.249 ):
-#          $cardinal = "ZW";
-#      break;
-#      case ( $degree >= 236.25 && $degree <= 258.749 ):
-#          $cardinal = "W ZW";
-#      break;
-#      case ( $degree >= 258.75 && $degree <= 281.249 ):
-#          $cardinal = "W";
-#      break;
-#      case ( $degree >= 281.25 && $degree <= 303.749 ):
-#          $cardinal = "W NW";
-#      break;
-#      case ( $degree >= 303.75 && $degree <= 326.249 ):
-#          $cardinal = "NW";
-#      break;
-#      case ( $degree >= 326.25 && $degree <= 348.749 ):
-#          $cardinal = "N NW";
-#      break;
-#      default:
-#          $cardinal = null;
-#  }
-# return $cardinal;
-#}
-#
-
-
 
 $datetime = date('Y-m-d H:i:s');
 $baromin = toHPA($_GET["baromin"]);
